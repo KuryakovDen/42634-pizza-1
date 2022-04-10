333e271b39f2f429f3385c210b2b37238bf2e383
KuryakovDen <fifer2@yandex.ru>
2022-04-10 21:24:38 +0300

    Подключение статических данных к компоненту

--- src/frontend/src/views/Index.vue
+++ src/frontend/src/views/Index.vue
@@ -1,11 +1,24 @@
 <template>
-    <p>123</p>
+  <div>
+    <p>{{ user.name }}</p>
+    <p>{{ pizza.dough[0].name }}</p>
+    <p>{{ misc[0].name }}</p>
+  </div>
 </template>
-
 <script>
+import user from "@/static/user.json";
+import pizza from "@/static/pizza.json";
+import misc from "@/static/misc.json";
 
+export default {
+  name: "Index",
+  data() {
+    return {
+      user,
+      pizza,
+      misc,
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
+<style></style>
